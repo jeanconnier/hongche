3a45d3e9f6325d3e73032f4c2f14900ea67f881d
jeanconnier <jean.connier@gmail.com>
2014-04-08 17:41:28 +0800

    Another carsale.sql fix
    Hash field length was 100, hash length 128

--- Design/carsale.sql
+++ Design/carsale.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 08, 2014 at 09:32 AM
+-- Generation Time: Apr 08, 2014 at 09:40 AM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.16
 
@@ -45,15 +45,7 @@ CREATE TABLE IF NOT EXISTS `demand` (
 --
 
 INSERT INTO `demand` (`DemandId`, `UserId`, `Brand`, `Type`, `Colour`, `Motor`, `State`, `isSecured`) VALUES
-('1', '13', 'Volkswagen', 'sedan', 'green', 'oil', 'second-hand', 0),
-('23211396886646', '2321', 'Dongfeng', 'Sedan', 'Black', 'Oil', 'New', 0),
-('23211396934439', '2321', 'Dongfeng', 'Sedan', 'Black', 'Oil', 'New', 0),
-('23211396935000', '2321', 'Dongfeng', 'Sedan', 'Black', 'Oil', 'New', 0),
-('23211396935004', '2321', 'Dongfeng', 'Sedan', 'Black', 'Oil', 'New', 0),
-('23211396935134', '2321', 'Dongfeng', 'Sport', 'Black', 'Oil', 'New', 0),
-('jean1396886895', 'jean', 'Toyota', 'Minivan', 'Black', 'Oil', 'New', 0),
-('michel1396882977', 'michel', 'Dongfeng', 'Sedan', 'Black', 'Oil', 'New', 0),
-('michel1396883018', 'michel', 'Saab', 'Minivan', 'Green', 'Oil', 'Second-hand', 0);
+('pc1396949879', 'pc', 'Citroen', 'Minivan', 'Blue', 'Oil', 'Second-hand', 0);
 
 -- --------------------------------------------------------
 
@@ -69,15 +61,6 @@ CREATE TABLE IF NOT EXISTS `offer` (
   PRIMARY KEY (`OfferId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `offer`
---
-
-INSERT INTO `offer` (`OfferId`, `DemandId`, `UserId`, `Price`) VALUES
-('1', '1', '63', 4000),
-('2', '1', '94', 6000),
-('29', '1', '13', 8000);
-
 -- --------------------------------------------------------
 
 --
@@ -87,7 +70,7 @@ INSERT INTO `offer` (`OfferId`, `DemandId`, `UserId`, `Price`) VALUES
 CREATE TABLE IF NOT EXISTS `user` (
   `UserId` varchar(100) NOT NULL,
   `UserName` varchar(100) NOT NULL,
-  `Hash` varchar(100) NOT NULL,
+  `Hash` varchar(512) NOT NULL,
   PRIMARY KEY (`UserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -96,8 +79,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`UserId`, `UserName`, `Hash`) VALUES
-('paul', 'Paul', '0ab8704c27fc675fc61de40329a38bb3ad81aadf8c03da1a1e9e7821fee1d675ac3c22d83746097e8a7932923067c615c166'),
-('pierre', 'Pierre', '2643b42c44b11a47e93bb68a62bc370fbb499b40feabd368041e0abe18d51578572cb6a65fc1790cf1af0a5f97a9d9eb4c05');
+('pc', 'portable', '40b244112641dd78dd4f93b6c9190dd46e0099194d5a44257b7efad6ef9ff4683da1eda0244448cb343aa688f5d3efd7314dafe580ac0bcbf115aeca9e8dc114');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
